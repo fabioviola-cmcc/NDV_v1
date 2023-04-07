fc0a39bb4485fab4a6c5ab999150be4b4e04372c
Afonso Gonçalves Neto <66923093+afonsogneto@users.noreply.github.com>
2023-04-07 09:11:18 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+conda create -n ndv_v1 matplotlib numpy munch xarray netCDF4 scipy basemap scikit-learn seaborn dask
+
+mkdir files
+mkdir output
+
+source activate ndv_v1
+
+pip install cmocean
+#conda install -c conda-forge basemap-data-hires
+
+conda deactivate
