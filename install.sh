bf910c739b970a4580fbccec0a113143822e161b
Afonso Gonçalves Neto <66923093+afonsogneto@users.noreply.github.com>
2023-05-26 08:48:19 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+module load anaconda
+conda create -n ndv_v1 matplotlib numpy munch xarray netCDF4 scipy basemap scikit-learn seaborn dask basemap-data-hires time
+
+mkdir files
+mkdir output
+
+source activate ndv_v1
+
+conda install -c conda-forge cmocean
+
+conda deactivate
